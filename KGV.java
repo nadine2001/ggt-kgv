cc0a8759d63ac75e71e2d9956937a4d541380442
nadine2001 <69674173+nadine2001@users.noreply.github.com>
2020-09-02 10:53:40 +0200

    Add files via upload

---
+++ KGV.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nadine
+ */
+public class KGV {
+    public static void (String args[]){
+         
+        Scanner eingabe = new Scanner(System.in);
+         
+        System.out.println("Was soll Ihre erste Zahl sein?");
+        int ersteZahl = eingabe.nextInt();
+        System.out.println("Was soll Ihre zweite Zahl sein?");
+        int zweiteZahl = eingabe.nextInt();
+         
+        int ersteZahlTemp = ersteZahl;
+        int zweiteZahlTemp = zweiteZahl;
+         
+        while (ersteZahlTemp != zweiteZahlTemp) {
+            if (ersteZahlTemp < zweiteZahlTemp) {
+                ersteZahlTemp += ersteZahl;
+            } else {
+                zweiteZahlTemp+= zweiteZahl;
+            }
+        }
+         
+        System.out.println("Der KGV ist: " + ersteZahlTemp);
+ 
+    }
+    
+}
+
